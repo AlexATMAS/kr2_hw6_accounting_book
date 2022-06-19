e8574f1f6398c7bee360e3979afa0ea0be1b0ff5
ALex ATMAS <atmasbooking@gmail.com>
2022-06-20 00:05:50 +0300

    final1

--- src/main/java/pro/sky/accounting_book/service/EmployeeService.java
+++ src/main/java/pro/sky/accounting_book/service/EmployeeService.java
@@ -13,15 +13,20 @@ import java.util.*;
 public class EmployeeService {
 
     private static final int LIMIT = 10;
-    private final List<Employee> employees = new ArrayList<>(10);
+    private final Map<String, Employee> employees = new HashMap<>();
+
+    private String getKey(Employee employee){
+        return employee.getFirstName()+"|" + employee.getLastName();
+    }
 
     public Employee addEmployees(String firstName, String lastName) {
         Employee employee = new Employee(firstName,lastName);
-        if(employees.contains(employee)){
+
+        if(employees.containsKey(employee)){
             throw new EmployeeAlreadyAddedException("Сотрудник уже числится в базе!");
         }
         if (employees.size()<LIMIT){
-            employees.add(employee);
+            employees.put(getKey(employee),employee);
             return employee;
         }else {
             throw new EmployeeStorageIsFullException("База данных переполнена!");
@@ -31,24 +36,24 @@ public class EmployeeService {
 
     public Employee deleteEmployee(String firstName, String lastName) {
         Employee employee = new Employee(firstName,lastName);
-        if (!employees.contains(employee)){
+        if (!employees.containsKey(getKey(employee))){
             throw new EmployeeNotFoundException("Сотрудник не найден!");
         }
-        employees.remove(employee);
+        employees.remove(getKey(employee));
         return employee;
     } // Удаление сотрудника;
 
 
     public Employee findEmployee (String firstName, String lastName)  {
         Employee employee = new Employee(firstName,lastName);
-        if (!employees.contains(employee)){
+        if (!employees.containsKey(getKey(employee))){
             throw new EmployeeNotFoundException("Сотрудник не найден!");
         }
         return employee;
     }//// Получить данные сотрудника по ФИО
 
     public  List<Employee> getAll(){
-        return new ArrayList<>(employees);
+        return new ArrayList<>(employees.values());
     }
 
 
